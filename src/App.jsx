f22fe36a8070c0cc16842884cadba7cea2375eee
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-06-29 13:35:31 +0530

    feat: Added the main css file and integrated Tailwind CSS and GSAP into the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { gsap } from "gsap";
 import{ ScrollTrigger, SplitText} from "gsap/all";
 //Gsap plugins are not registered by default, so we need to register them manually
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='flex-center'>
+      <h1 className='text-4xl text-yellow-500'>heyyyy</h1>
     </div>
   )
 }
