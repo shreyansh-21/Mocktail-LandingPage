d22ed8a2efb019562b6cad48ba349d256a931018
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-06-29 13:44:55 +0530

    feat: Add Navbar component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { gsap } from "gsap";
 import{ ScrollTrigger, SplitText} from "gsap/all";
+import Navbar from './components/Navbar';
 //Gsap plugins are not registered by default, so we need to register them manually
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className='flex-center'>
-      <h1 className='text-4xl text-yellow-500'>heyyyy</h1>
-    </div>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
