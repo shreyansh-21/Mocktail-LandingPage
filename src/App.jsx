027091af6c91eb073e3ac54d5bb37e13077410ef
Shreyansh Dubey <shreyansh2128@gmail.com>
2025-06-29 21:46:27 +0530

    feat: Add About component with GSAP animations and content

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import{ ScrollTrigger, SplitText} from "gsap/all";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import About from './components/About';
 //Gsap plugins are not registered by default, so we need to register them manually
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,6 +14,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Cocktails/>
+      <About/>
     </main>
   )
 }
